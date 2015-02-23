f3cab50cd23bba95cf95aa2e4a9f11aada47912e
Helder Santana <contato@heldersantana.net>
2015-02-22 23:02:18 -0300

    Word Generator Service Implementation

--- src/Hangman/Provider/WordServiceProvider.php
+++ src/Hangman/Provider/WordServiceProvider.php
@@ -9,7 +9,20 @@ class WordServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['word.new'] = $app->protect(function () use ($app) {});
+        $app['word.new'] = $app->protect(function () use ($app) {
+            $position = rand(0, 274907);
+
+            $handle = @fopen($app['word.path'], "r");
+
+            $line = 1;
+            while (($buffer = fgets($handle)) !== false && $line < $position) {
+                $line++;
+            }
+
+            fclose($handle);
+
+            return $buffer;
+        });
     }
 
     public function boot(Application $app)
